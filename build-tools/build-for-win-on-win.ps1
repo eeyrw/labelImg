c7cab25c64fedd8e488f3653476be32a9753d8a6
yuan <ayrw@163.com>
2023-06-17 20:49:32 +0800

    M

--- build-tools/build-for-win-on-win.ps1
+++ build-tools/build-for-win-on-win.ps1
@@ -1,5 +1,11 @@
+Remove-Item -Path "build" -Recurse
+Remove-Item -Path "dist" -Recurse
+Remove-Item -Path labelImg.spec
+
 pyinstaller --hidden-import=xml `
             --hidden-import=xml.etree `
             --hidden-import=xml.etree.ElementTree `
             --hidden-import=lxml.etree `
              -D -F -n labelImg -c "../labelImg.py" -p ../libs -p ../
+
+Copy-Item "../data" -Destination "dist/" -Recurse
